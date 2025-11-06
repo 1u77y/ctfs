686185adbb901e296045f8c7ad433a0d6e84a14b
mhacxermood914 <soultonew2@gmail.com>
2025-11-06 15:54:46 +0100

    feat: last update

--- public-app/app.py
+++ public-app/app.py
@@ -197,7 +197,7 @@ def fetch():
             base_tag = f'<base href="/fetch?url={quote(target_url, safe="")}" />'
             text = re.sub(r'(?i)<head\b[^>]*>', lambda m: m.group(0) + base_tag, text, count=1)
 
-            def rewrite_static(m):
+            # def rewrite_static(m):
                 # url_ = m.group("url").lstrip('/')
                 # full_url = (
                 #     f"http://{hostname}:9000/openapi/{url_}" if url_.startswith("swagger")
@@ -205,11 +205,11 @@ def fetch():
                 # )
                 # return f'{m.group("prefix")}/fetch?url=' + quote(full_url, safe='')
 
-            text = re.sub(
-                r'(?P<prefix>(?:src|href)=["\'])(?P<url>(?:swagger|flasgger_static|static)/[^"\']+)',
-                rewrite_static,
-                text
-            )
+            # text = re.sub(
+            #     r'(?P<prefix>(?:src|href)=["\'])(?P<url>(?:swagger|flasgger_static|static)/[^"\']+)',
+            #     rewrite_static,
+            #     text
+            # )
 
             text = re.sub(
                 r"<script.*?>.*?</script>",
